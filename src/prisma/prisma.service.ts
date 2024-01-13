645bb719623f3a5faed84906ce6c057ad38b8720
ben <benjamin.baret@edu.ece.fr>
2024-01-13 19:55:17 +0100

    chore: initiate repo with nestjs tutorial

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable( )
+export class PrismaService extends PrismaClient{
+    constructor(config: ConfigService) {
+        super({
+            datasources: {
+                db: {
+                    url: config.get('DATABASE_URL')
+                },
+            },
+        });
+    }
+
+    // tear down logic before e2e tests
+    cleanDb() {
+        return this.$transaction([
+            this.bookmark.deleteMany(),
+            this.user.deleteMany(),
+        ]);
+    }
+}
---
+++ src/user/user.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get, UseGuards, Patch, Body } from '@nestjs/common';
+import { JwtGuard } from '../../src/auth/guard';
+import { GetUser } from '../../src/auth/decorator';
+import { User } from '@prisma/client';
+import { EditUserDto } from './dto/edit-user.dto';
+import { UserService } from './user.service'
+
+@UseGuards(JwtGuard)
+@Controller('users')
+export class UserController {
+    constructor(private userService: UserService) {}
+    @Get('me') 
+    getMe(@GetUser('') user: User) {
+        return user;
+    }
+
+    @Patch()
+    async editUser(
+        @GetUser('id') userId: number,
+        @Body() dto: EditUserDto
+        ){
+        return this.userService.editUser(userId, dto);
+    }
+    
+}
