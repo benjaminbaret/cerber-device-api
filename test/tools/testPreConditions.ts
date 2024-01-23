dfd7c8197c4407f422bd7b2b5f33e8020777c01f
benjaminbaret <benjamin.baret@edu.ece.fr>
2024-01-23 22:40:40 +0100

    fix: prod url in e2e tests

--- test/tools/testPreConditions.ts
+++ test/tools/testPreConditions.ts
@@ -36,7 +36,7 @@ export async function testPreConditions(): Promise<void> {
     const update = await prisma.update.create({
         data: {
             name: "Raspberry udpate 1",
-            url: "https://147.135.129.16:9000/test-nico/bundle.raucb?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=3O9B1UQ1J4KWZQAU6FGQ%2F20240122%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240122T102107Z&X-Amz-Expires=43200&X-Amz-Security-Token=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NLZXkiOiIzTzlCMVVRMUo0S1daUUFVNkZHUSIsImV4cCI6MTcwNTk1NTc1MCwicGFyZW50Ijoicm9vdElzUm9vdCJ9.b4YoVJjrjaLjSM_rri7n7TpEE43tj-J7zByQnwHPmEjtisYcyR8DeZNwUTt9g4TkaGjSHzzejw_E8PVOBGccrA&X-Amz-SignedHeaders=host&versionId=null&X-Amz-Signature=934a6e3f05940ad16e41b1aa2583d9ef4e763efdbc7468a13b4a9ed7c23a0c38",
+            url: "https://example.com",
             size: "100",
         },
     }); 
